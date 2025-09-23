70a24ab9a2334ba0e16894e17bcc1118179707c6
Shareiar <islamshareiar@gmail.com>
2025-09-24 03:39:52 +0600

    change some ui design at client end

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
+import PrivateRoute from './components/PrivateRoute';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/admin/login" element={<AdminLogin />} />
-      <Route path="/admin" element={<AdminDashboard />} />
+      <Route
+        path="/admin/*"
+        element={
+          <PrivateRoute>
+            <AdminDashboard />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   </BrowserRouter>
 );
