d78e76635416751fa4b5bd6ef53bd589fd4a0ad2
scottShams <scott123.staff.india@gmail.com>
2025-09-24 22:17:13 +0600

    update: add skilss route

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/skills', require('./routes/skills'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
