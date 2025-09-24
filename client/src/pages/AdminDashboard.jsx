e2ce88abc140237944983b14b4796089b108aef6
scottShams <scott123.staff.india@gmail.com>
2025-09-24 22:37:01 +0600

    update: import projectsManagement pages from admin folder

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AdminLayout from '../components/admin/AdminLayout';
+import ProjectsManagement from './admin/ProjectsManagement';
 import api from '../utils/api';
 import { Activity, Briefcase, Code2, User } from 'lucide-react';
 
