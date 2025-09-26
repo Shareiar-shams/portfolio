12f59a5855030bf80ea589855848c0fc2d7bee18
scottShams <scott123.staff.india@gmail.com>
2025-09-26 16:39:12 +0600

    update: Remove unused assigned value

--- admin-app/src/pages/admin/about/AboutFormPage.jsx
+++ admin-app/src/pages/admin/about/AboutFormPage.jsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import AboutForm from '../../../pages/forms/about/AboutForm';
 import api from '../../../utils/api';
 
 export default function AboutFormPage() {
-  const navigate = useNavigate();
   const [about, setAbout] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
