e8454f0eb92105a9a4367c8ac10f7d5211d28ec4
Shareiar <islamshareiar@gmail.com>
2025-09-27 05:59:39 +0600

    add location document in collection

--- backend/models/Experience.js
+++ backend/models/Experience.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const ExperienceSchema = new mongoose.Schema({
   company: { type: String, required: true },
   position: { type: String, required: true },
+  location: { type: String },
   startDate: { type: Date, required: true },
   endDate: { type: Date },
   current: { type: Boolean, default: false },
