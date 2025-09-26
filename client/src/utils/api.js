af9a1b5119dbced75a94df31d00760b3fb35317e
Shareiar <islamshareiar@gmail.com>
2025-09-27 05:29:21 +0600

    remove client folder instead off using admin-app folder for admin dashboard

--- client/src/utils/api.js
+++
@@ -1,5 +0,0 @@
-// client/utils/api.js
-import axios from 'axios';
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
-const api = axios.create({ baseURL });
-export default api;
