c3e7be2a48d327a9e636018ccee8563285b93a5b
Shareiar <islamshareiar@gmail.com>
2025-09-27 16:07:33 +0600

    Create visitor model

---
+++ backend/models/Visitor.js
@@ -0,0 +1,8 @@
+// backend/models/Visitor.js
+const mongoose = require('mongoose');
+
+const VisitorSchema = new mongoose.Schema({
+  count: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model('Visitor', VisitorSchema);
