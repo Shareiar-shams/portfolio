0e48e5509f03daae812c83defa596ab62f51257f
scottShams <scott123.staff.india@gmail.com>
2025-09-30 14:21:22 +0600

    remove require dotenv

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-require('dotenv').config();
+// require('dotenv').config();
 
 const app = express();
 connectDB();
