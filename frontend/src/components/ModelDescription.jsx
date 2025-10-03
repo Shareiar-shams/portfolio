5645b714e2d8ae7dbc992f84d20cfd45a444481d
scottShams <scott123.staff.india@gmail.com>
2025-10-03 21:38:41 +0600

    create: by using this componant read html speacil char data

---
+++ frontend/src/components/ModelDescription.jsx
@@ -0,0 +1,26 @@
+import DOMPurify from "dompurify";
+
+function fixQuillLists(html) {
+  let fixed = html;
+
+  // Convert <ol><li data-list="bullet"> into <ul><li>
+  fixed = fixed.replace(/<ol[^>]*>\s*<li[^>]*data-list="bullet"[^>]*>/g, "<ul><li>");
+  
+  // Convert subsequent <li data-list="bullet"> into normal <li>
+  fixed = fixed.replace(/<li[^>]*data-list="bullet"[^>]*>/g, "<li>");
+
+  // Close </ul> instead of </ol> where bullets were used
+  fixed = fixed.replace(/<\/ol>/g, "</ul>");
+
+  return fixed;
+}
+
+export default function ModelDescription({ description }) {
+  const cleanHtml = DOMPurify.sanitize(fixQuillLists(description));
+  return (
+    <div
+      className="prose prose-sm text-gray-400 mt-2 max-w-full"
+      dangerouslySetInnerHTML={{ __html: cleanHtml }}
+    />
+  );
+}
